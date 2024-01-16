44ad4da6c5a408fdcde490cd65294e1d8c90e8a0
volvic0071 <149310872+volvic0071@users.noreply.github.com>
2024-01-17 00:58:58 +0900

    gpt-3.5-turbo-1106
    gpt-3.5-turbo-1106へ変更

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def communicate():
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
-        model="gpt-4-1106-preview",
+        model="gpt-3.5-turbo-1106",
         messages=messages
     )
 
